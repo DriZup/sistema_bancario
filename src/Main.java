9c72efa62ef5cac72cda850811b0a74fffa7146d
adriana.nogueira <adriana.nogueira@zup.com.br>
2025-01-08 20:55:37 -0300

    feat: criar classe Main para testar a ContaCorrente com dependências e transações"

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
+import calculadoras.CalculadoraTaxaCorrente;
+import contas.ContaCorrente;
+import notificacoes.NotificadorEmail;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+
+        CalculadoraTaxaCorrente calculadoraTaxaCorrente = new CalculadoraTaxaCorrente();
+
+        NotificadorEmail notificadorEmail = new NotificadorEmail();
+
+        ContaCorrente contaCorrente = new ContaCorrente(1000.0, calculadoraTaxaCorrente, notificadorEmail);
+
+        contaCorrente.realizarTransacao(100.0);
+        System.out.println("Saldo após transação: " + contaCorrente.getSaldo());
+        System.out.println("Taxa da Conta Corrente: " + contaCorrente.calcularTaxa());
     }
 }
