aff89808adad7edfcce280daa2fc3eb9bbf440d4
adriana.nogueira <adriana.nogueira@zup.com.br>
2025-01-08 20:57:06 -0300

    feat: criar classe Main para testar a contaPoupança com dependências e transações.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 import calculadoras.CalculadoraTaxaCorrente;
 import contas.ContaCorrente;
+import contas.ContaPoupanca;
 import notificacoes.NotificadorEmail;
 
 public class Main {
@@ -10,9 +11,14 @@ public class Main {
         NotificadorEmail notificadorEmail = new NotificadorEmail();
 
         ContaCorrente contaCorrente = new ContaCorrente(1000.0, calculadoraTaxaCorrente, notificadorEmail);
+        ContaPoupanca contaPoupanca = new ContaPoupanca(1000.0, calculadoraTaxaCorrente, notificadorEmail);
 
         contaCorrente.realizarTransacao(100.0);
+        contaPoupanca.realizarTransacao(50.0);
         System.out.println("Saldo após transação: " + contaCorrente.getSaldo());
         System.out.println("Taxa da Conta Corrente: " + contaCorrente.calcularTaxa());
+
+        System.out.println("Saldo da Conta Poupança: " + contaPoupanca.getSaldo());
+        System.out.println("Taxa da Conta Poupança: " + contaPoupanca.calcularTaxa());
     }
 }
