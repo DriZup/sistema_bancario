785386dd5d72bed73699f1500c46c6e22e8e096e
adriana.nogueira <adriana.nogueira@zup.com.br>
2025-01-09 09:22:09 -0300

    feat: Atualizações no código principal para suportar diferentes tipos de contas e calculadoras de taxas

--- src/Main.java
+++ src/Main.java
@@ -1,24 +1,30 @@
 import calculadoras.CalculadoraTaxaCorrente;
+import calculadoras.CalculadoraTaxaPoupanca;
+import calculadoras.ICalculadorTaxas;
 import contas.ContaCorrente;
 import contas.ContaPoupanca;
+import notificacoes.INotificador;
 import notificacoes.NotificadorEmail;
 
 public class Main {
     public static void main(String[] args) {
+        ICalculadorTaxas calculadora = new CalculadoraTaxaCorrente();
+        ICalculadorTaxas calculadoraPoupanca = new CalculadoraTaxaPoupanca();
+        INotificador notificador = new NotificadorEmail();
 
-        CalculadoraTaxaCorrente calculadoraTaxaCorrente = new CalculadoraTaxaCorrente();
+        ContaCorrente contaCorrente = new ContaCorrente(1000.0, calculadora, notificador);
+        ContaPoupanca contaPoupanca = new ContaPoupanca(1000.0, calculadoraPoupanca, notificador);
 
-        NotificadorEmail notificadorEmail = new NotificadorEmail();
+        contaCorrente.realizarTransacao(200.0);
+        contaCorrente.realizarTransacao(-150.0);
 
-        ContaCorrente contaCorrente = new ContaCorrente(1000.0, calculadoraTaxaCorrente, notificadorEmail);
-        ContaPoupanca contaPoupanca = new ContaPoupanca(1000.0, calculadoraTaxaCorrente, notificadorEmail);
+        contaPoupanca.realizarTransacao(100.0);
+        contaPoupanca.realizarTransacao(-50.0);
 
-        contaCorrente.realizarTransacao(100.0);
-        contaPoupanca.realizarTransacao(50.0);
-        System.out.println("Saldo após transação: " + contaCorrente.getSaldo());
-        System.out.println("Taxa da Conta Corrente: " + contaCorrente.calcularTaxa());
+        System.out.println("Saldo final: " + contaCorrente.getSaldo());
+        System.out.println("Taxa calculada: " + contaCorrente.calcularTaxa());
 
-        System.out.println("Saldo da Conta Poupança: " + contaPoupanca.getSaldo());
-        System.out.println("Taxa da Conta Poupança: " + contaPoupanca.calcularTaxa());
+        System.out.println("Saldo final contaPoupança: " + contaPoupanca.getSaldo());
+        System.out.println("Taxa calculada conataPoupança: " + contaPoupanca.calcularTaxa());
     }
 }
