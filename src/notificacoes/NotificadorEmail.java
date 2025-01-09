63c398f2620b480727218211f2b84053da057cf1
adriana.nogueira <adriana.nogueira@zup.com.br>
2025-01-09 09:17:39 -0300

    refactor: Implementado envio de notificações no NotificadorEmail.

--- src/notificacoes/NotificadorEmail.java
+++ src/notificacoes/NotificadorEmail.java
@@ -3,14 +3,12 @@ package notificacoes;
 public class NotificadorEmail implements INotificador {
 
 
-    @Override
-    public void notificar(String mensagem) {
-        System.out.println();
 
-    }
 
     @Override
     public void enviarNotificacao(String mensagem) {
+        System.out.println("Enviando e-mail: " + mensagem);
+
 
     }
 }
